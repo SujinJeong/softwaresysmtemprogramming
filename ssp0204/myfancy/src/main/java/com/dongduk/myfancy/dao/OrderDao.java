e225242ae6937986f0f153ef8a6ac159c116ca32
sunhee <sunhee@DESKTOP-EJD4FC3>
2020-06-11 15:29:12 +0900

    OrderController, OrderService

--- ssp0204/myfancy/src/main/java/com/dongduk/myfancy/dao/OrderDao.java
+++ ssp0204/myfancy/src/main/java/com/dongduk/myfancy/dao/OrderDao.java
@@ -7,7 +7,7 @@ import org.springframework.dao.DataAccessException;
 import com.dongduk.myfancy.domain.Order;
 import com.dongduk.myfancy.domain.Order_product;
 
-public interface OrderDao {
+public interface OrderDao { //Service에 의해 호출되어 DB CRUD를 담당,,단일 데이터의 접근과 갱신만 처리한다(CRUD)
 
 	List<Order> getOrderListByStore(int store_id) throws DataAccessException;
 	// 지점에서 발주 신청한 목록 리스트
@@ -15,5 +15,5 @@ public interface OrderDao {
 	// 발주 조회 ex. 발주 번호 3 : 펜1 지우개 2
 	void insertOrder(List<Order_product> orderProductList, int store_id, int amount) throws DataAccessException; 
 	// 발주 등록
-
+	
 }
--- ssp0204/myfancy/src/main/java/com/dongduk/myfancy/domain/Cart.java
+++ ssp0204/myfancy/src/main/java/com/dongduk/myfancy/domain/Cart.java
@@ -2,32 +2,41 @@ package com.dongduk.myfancy.domain;
 
 import java.util.Map;
 
+
 public class Cart {
+	
 	private Map<Product, Integer> cartList;
 
 	public Cart() {
 		super();
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	public Map<Product, Integer> getCartList() {
 		return cartList;
 	}
 	
-	void addProduct(int product_id, int quantity) {
+	public void addProductForOrder(Product product, int quantity) { // cart에 담기
+		cartList.put(product, quantity);
+	}
+	
+	public void checkInStock(Map list) {
 		
 	}
 	
-	void checkInStock(Map list) {
-		
+	public void setQuantityByProductId(int product_id) {
+
 	}
 	
-	void setQuantityByProductId(int product_id) {
-		
-	}
-	
-	int getSubTotal() {
-		return 0;
+	public int getSubOrderTotal(Product product) { // 카트에 담긴 총 금액
+		int total = 0;
+		Map<Product, Integer> cartList = getCartList(); // cart에 담긴 물품들
+		for(Map.Entry<Product, Integer> elem : cartList.entrySet()) { // cart에 담긴 물품들 발주
+			int order_product_quantity = elem.getValue();
+			int order_price = product.getOrder_price();
+			total += order_price * order_product_quantity;
+		}
+		return total;
 	}
 
 }
